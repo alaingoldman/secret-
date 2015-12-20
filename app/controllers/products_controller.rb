ddb55e35dee7cbb10c62044408ef125009811ce1
Alain <alain.goldman@gmail.com>
2015-12-20 07:42:46 +0000

    started adding products model to db

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,44 @@
+class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+        flash[:green] = "Success"
+        redirect_to product_path(@product)
+    else
+        flash[:red] = @product.errors.full_messages.first
+        render "new"
+    end
+  end
+  
+  def index
+      @products = Product.all
+  end
+
+  def show
+      @product = Product.find(params[:id])
+  end
+
+  def edit 
+      @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+        flash[:green] = "edit made"
+        redirect_to product_path(@product)
+    else
+        flash[:red] = @product.errors.full_messages.first
+        render "edit"
+    end
+  end
+  
+  private 
+    def product_params
+        params.require(:product).permit(:title, :category, :description, :price)
+    end
+end
